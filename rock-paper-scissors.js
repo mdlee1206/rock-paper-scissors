7379f91ae9664bb33198ab159c7b0c78ec1d1d6c
Marc Lee <mdlee1206@gmail.com>
2024-02-20 20:20:17 -0800

    Create function playRound()

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,9 +9,32 @@ function getPlayerChoice() {
     const acceptableChoices = ['rock', 'paper', 'scissors'];
 
     if (acceptableChoices.includes(choice)) {
-        return choice
+        return choice.charAt(0).toUpperCase() + choice.substr(1).toLowerCase();
     } else {
         alert('Not a valid choice. Pick again.')
         getPlayerChoice();
+    }
+}
+
+function playRound(getPlayerChoice, getComputerChoice) {
+    const playerChoice = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+
+    if (playerChoice === computerChoice) {
+        return `It's a tie! Both selected ${playerChoice}.`;
+    } else if (playerChoice === 'Rock' && computerChoice === 'Paper') {
+        return `You lose! ${computerChoice} beats ${playerChoice}.`;
+    } else if (playerChoice === 'Rock' && computerChoice === 'Scissors') {
+        return `You win! ${playerChoice} beats ${computerChoice}.`;
+    } else if (playerChoice === 'Scissors' && computerChoice === 'Rock') {
+        return `You lose! ${computerChoice} beats ${playerChoice}.`;
+    } else if (playerChoice === 'Scissors' && computerChoice === 'Paper') {
+        return `You win! ${playerChoice} beats ${computerChoice}.`;
+    } else if (playerChoice === 'Paper' && computerChoice === 'Rock') {
+        return `You win! ${playerChoice} beats ${computerChoice}.`;
+    } else if (playerChoice === 'Paper' && computerChoice === 'Scissors') {
+        return `You lose! ${computerChoice} beats ${playerChoice}.`;
+    } else {
+        return `You selected ${playerChoice}, and the computer selected ${computerChoice}.`
     }
 }
