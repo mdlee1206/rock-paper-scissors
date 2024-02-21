33464e2f7b2a191b3dd9c739936d1bea0da612bd
Marc Lee <mdlee1206@gmail.com>
2024-02-20 20:05:06 -0800

    Create function getPlayerChoice()

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,3 +3,15 @@ function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
+
+function getPlayerChoice() {
+    let choice = prompt('Pick rock, paper, or scissors: ').toLowerCase();
+    const acceptableChoices = ['rock', 'paper', 'scissors'];
+
+    if (acceptableChoices.includes(choice)) {
+        return choice
+    } else {
+        alert('Not a valid choice. Pick again.')
+        getPlayerChoice();
+    }
+}
